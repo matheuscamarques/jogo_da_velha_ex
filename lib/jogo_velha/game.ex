58712f6d5b3e03242a70c82847e1795c4311dab2
Matheus C Marques <matheuscamarques@gmail.com>
2024-02-07 23:36:52 -0300

    feat: last moves and active player logic

--- lib/jogo_velha/game.ex
+++ lib/jogo_velha/game.ex
@@ -6,13 +6,16 @@ defmodule JogoVelha.Game do
       player1: nil,
       player2: nil,
       board: Board.new(),
-      game_state: :new
+      game_state: :new,
+      last_moves: [],
+      active_player: nil
     }
   end
 
   def set_player1(game, player) do
     game
     |> Map.put(:player1, player)
+    |> Map.put(:active_player, player)
   end
 
   def set_player2(game, player) do
@@ -20,17 +23,34 @@ defmodule JogoVelha.Game do
     |> Map.put(:player2, player)
   end
 
-  def move(game = %{board: board}, %{x: _, y: _} = position, player) do
+  def move(
+        game = %{board: board, active_player: player, player1: player1, player2: player2},
+        %{x: _, y: _} = position,
+        player
+      ) do
     new_board = Map.put(board, position, %{state: player})
 
     new_game_state = Board.verify(new_board)
 
+    next_player_turn =
+      case player == player1 do
+        true -> player2
+        false -> player1
+      end
+
     new_game =
       game
+      |> Map.put(:last_moves, [board] ++ game.last_moves)
       |> Map.put(:board, new_board)
       |> Map.put(:game_state, new_game_state)
+      |> Map.put(:active_player, next_player_turn)
 
     Board.print_board(new_board)
     new_game
+  end
+
+  def move(game, _position, _player) do
+    IO.inspect("Error: Wrong Player Turn")
+    {:wrong_player_turn, game}
   end
 end
